c66b2486376d98ae314d8c2a2a2ad9878c3c7fa5
Abubakar <edwardian.edu02@gmail.com>
2021-06-03 15:06:29 -0700

    initial commit

---
+++ js/dist/index.dev.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// alert('Welocme here');
+// var btn1 = document.getElementById('btn');
+// btn1.addEventListener('click', () =>{
+//     document.getElementsByClassName("bg-text").style.display = "block";
+// });
+var clickme = function clickme() {
+  var Tome = document.getElementById('text');
+
+  if (Tome.style.display === 'none') {
+    Tome.style.display = 'block';
+  } else {
+    Tome.style.display = 'none';
+  }
+};
+
+function clickmeagain() {
+  var Tomee = document.getElementById('textt');
+
+  if (Tomee.style.display === 'none') {
+    Tomee.style.display = 'block';
+  } else {
+    Tomee.style.display = 'none';
+  }
+}
+
+function clickmehere() {
+  var me = document.getElementById('tex');
+
+  if (me.style.display === 'none') {
+    me.style.display = 'block';
+  } else {
+    me.style.display = 'none';
+  }
+}
+
+var clickmehereagian = function clickmehereagian() {
+  var me = document.getElementById('texx');
+
+  if (me.style.display === 'none') {
+    me.style.display = 'block';
+  } else {
+    me.style.display = 'none';
+  }
+};
+
+var justClick = function justClick() {
+  var mee = document.getElementById('texxx');
+
+  if (mee.style.display === 'none') {
+    mee.style.display = 'block';
+  } else {
+    mee.style.display = 'none';
+  }
+};
+
+function justClickme() {
+  var mee = document.getElementById('YourDesire');
+
+  if (mee.style.display === 'none') {
+    mee.style.display = 'block';
+  } else {
+    mee.style.display = 'none';
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,83 @@
+// alert('Welocme here');
+// var btn1 = document.getElementById('btn');
+// btn1.addEventListener('click', () =>{
+//     document.getElementsByClassName("bg-text").style.display = "block";
+    // });
+    const clickme = () =>
+    {
+            var Tome = document.getElementById('text');
+            if(Tome.style.display === 'none')
+            {
+                Tome.style.display = 'block';
+            }
+            else 
+            {
+                Tome.style.display = 'none';
+            }
+            
+    }
+
+    function clickmeagain(){
+        let Tomee = document.getElementById('textt');
+            if(Tomee.style.display === 'none')
+            {
+                Tomee.style.display = 'block';
+            }
+            else 
+            {
+                Tomee.style.display = 'none';
+            }
+    }
+
+    function clickmehere()
+    {
+            var me = document.getElementById('tex');
+            if(me.style.display === 'none')
+            {
+                me.style.display = 'block';
+            }
+            else 
+            {
+                me.style.display = 'none';
+            }
+            
+    }
+
+    let clickmehereagian = () =>
+    {
+            var me = document.getElementById('texx');
+            if(me.style.display === 'none')
+            {
+                me.style.display = 'block';
+            }
+            else 
+            {
+                me.style.display = 'none';
+            }
+            
+    }
+    
+    let justClick = () =>
+    {
+            let mee = document.getElementById('texxx');
+            if(mee.style.display === 'none')
+            {
+                mee.style.display = 'block';
+            }
+            else 
+            {
+                mee.style.display = 'none';
+            }
+            
+    }
+    function justClickme() {
+        let mee = document.getElementById('YourDesire');
+        if(mee.style.display === 'none')
+        {
+            mee.style.display = 'block';
+        }
+        else 
+        {
+            mee.style.display = 'none';
+        }
+    }
